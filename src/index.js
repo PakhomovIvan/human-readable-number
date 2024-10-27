5c55575b84dd61516719a00e13be0fac3fa972c7
Ivan Pakhomov <ip1133@ya.ru>
2024-10-28 00:05:04 +0300

    feat: solved 70% & error 90*

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const result = Array.from(String(number));
+    const num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const nTy = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number < 20) {
+        return num[number];
+    } else if (number < 100) {
+        if (number % 10 == 0) {
+            return nTy[result[0] - 2];
+        } else {
+            return `${nTy[result[0] - 2]} ${num[result[1]]}`;
+        }
+    } else if (number >= 100 && number < 1000) {
+        if (number == 100) return `one hundred`;
+        if (number % 100 == 0) {
+            return `${num[result[0]]} hundred`;
+        } else {
+            return `${num[result[0]]} hundred ${toReadable(
+                result.slice(1).join("")
+            )}`;
+        }
+    }
+};
+
+// console.log(toReadable(901))
